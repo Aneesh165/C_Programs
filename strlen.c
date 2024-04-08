730a452347fae543ac497ea7a3849da5c58775b4
Aneesh165 <aneeshmp165@gmail.com>
2024-04-08 12:24:19 +0530

    08-04-2024

---
+++ strlen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	printf("String Length is %d",strlen(string));
+	
+	printf("\nString Length is %d",sizeof(string));
+}
