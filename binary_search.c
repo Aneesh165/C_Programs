109525abd8a4b61ae585c84cd0a4cf06ae9363f7
Aneesh165 <aneeshmp165@gmail.com>
2024-04-09 13:12:36 +0530

    08-04-2024

---
+++ binary_search.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int arr[]={2,3,4,5,6,7,8,9};
+	int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 4;
+    int low = 0;
+    int high = n - 1;
+    int result=-1;
+    
+    while(low<=high)
+    {
+    	int mid=low+(high-low)/2;
+    	 if (arr[mid] == key)
+		  {
+            result = mid;
+            break;
+		  }
+         if (arr[mid] < key)
+            low = mid + 1; 
+        else
+            high = mid - 1;
+    }
+
+    if (result == -1)
+        printf("Element not present in array\n");
+    else
+        printf("Element found at index: %d\n", result);
+
+}
+
